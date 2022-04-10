cdf8a0747c0aae5998bae95a8b45e0bd667f6844
pignoli <wmishima@gmail.com>
2022-04-10 09:02:10 +0900

    4-6

--- book/urls.py
+++ book/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('book/',views.ListBookView.as_view())
+    path('book/',views.ListBookView.as_view()),
+    path('book/<int:pk>/detail/',views.DetailBookView.as_view()),
 ]
