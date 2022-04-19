8cd1be00b4f7a69e7fb5ed7d74567702d908ffd3
pignoli <wmishima@gmail.com>
2022-04-19 21:27:46 +0900

    5-10

--- book/views.py
+++ book/views.py
@@ -1,27 +1,28 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render,redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
 from .models import Book, Review
 
-class ListBookView(ListView):
+class ListBookView(LoginRequiredMixin,ListView):
     template_name='book/book_list.html'
     model=Book
-class DetailBookView(DetailView):
+class DetailBookView(LoginRequiredMixin,DetailView):
     template_name='book/book_detail.html'
     model=Book
 
-class CreateBookView(CreateView):
+class CreateBookView(LoginRequiredMixin,CreateView):
     template_name='book/book_create.html'
     model=Book
     fields=('title','text','category','thumbnail')
     success_url=reverse_lazy('list-book')
 
-class DeleteBookView(DeleteView):
+class DeleteBookView(LoginRequiredMixin,DeleteView):
     template_name='book/book_confirm_delete.html'
     model=Book
     success_url=reverse_lazy('list-book')
 
-class UpdateBookView(UpdateView):
+class UpdateBookView(LoginRequiredMixin,UpdateView):
     template_name='book/book_update.html'
     model=Book
     fields=('title','text','category','thmbnail','thumbnail')
@@ -30,7 +31,7 @@ class UpdateBookView(UpdateView):
 def index_view(request):
     object_list=Book.objects.order_by('category')
     return render(request, 'book/index.html',{'object_list': object_list})
-class CreateReviewView(CreateView):
+class CreateReviewView(LoginRequiredMixin,CreateView):
     model=Review
     fields=('book','title','text','rate','thumbnail')
     template_name='book/review_form.html'
